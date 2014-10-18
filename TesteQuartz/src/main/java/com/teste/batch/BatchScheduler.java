73364f8597eafd28712bf35136ff1c205f19351b
Diego <diegofeijo.silva@gmail.com>
2014-10-17 23:42:39 -0300

    Nova alteração;

--- TesteQuartz/src/main/java/com/teste/batch/BatchScheduler.java
+++ TesteQuartz/src/main/java/com/teste/batch/BatchScheduler.java
@@ -15,10 +15,11 @@ public class BatchScheduler {
 		// Detalhes da tarefa
         JobDetail job = JobBuilder.newJob(BatchJob.class).withIdentity(
                 "tarefaAloMundo", "group1").build();
-        // Gatilho - ou seja, quando irá chamar, neste caso, a cada 5 segundos
+        
+        // Gatilho - Faz a chamada todos os dias as 23:38
         Trigger trigger = TriggerBuilder.newTrigger().withIdentity(
                 "gatilhoAloMundo", "group1").withSchedule(
-                CronScheduleBuilder.cronSchedule("0/5 * * * * ?")).build();
+                CronScheduleBuilder.cronSchedule("0 42 23 * * ?")).build();
  
         // Agenda e voa lá!
         Scheduler scheduler = new StdSchedulerFactory().getScheduler();
@@ -26,5 +27,23 @@ public class BatchScheduler {
         scheduler.scheduleJob(job, trigger);
 		
 	}
+	
+	/*
+	 * Para formar uma expressão cron, alguns requisitos devem ser satisfeitos. 
+	 * Primeiramente, deve-se conhecer a ordem dos parâmetros:
+
+			1. Segundos;
+			2. Minutos;
+			3. Horas;
+			4. Dia do mês;
+			5. Mês;
+			6. Dia da semana;
+			7. Ano (opcional).
+			
+			Por exemplo, uma expressão cron que será executada todas às quartas-feiras às 12:00pm é escrita da seguinte forma: “0 0 12 ? * WED”. 
+			Outro exemplo de expressão demonstra uma tarefa executada todos os dias, às 08:00am e 12:00pm: “0 0 8,12 * * *”.
+			
+			Fonte: http://imasters.com.br/artigo/23593/java/entenda-como-funciona-o-quartz-enterprise-job-scheduler/
+	 * */
 	
 }
